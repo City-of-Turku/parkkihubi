65420e5dd2304f7e3eb199a59e3309bfa901f65c
Jonne Kokkonen <jonne.kokkonen@gofore.com>
2025-11-14 15:23:06 +0200

    TKUAOK-33 update react to 19, update redux

--- dashboard/src/store.ts
+++ dashboard/src/store.ts
@@ -1,32 +1,19 @@
-import {
-  applyMiddleware,
-  compose,
-  createStore,
-  Store,
-  Middleware,
-} from 'redux';
-import thunk from 'redux-thunk';
-
+import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
-import { RootState } from './types';
 import rootReducer from './reducers';
 import * as config from './config';
 
-const middlewares: Middleware[] = [thunk];
-if (config.isDev) {
-  middlewares.push(createLogger() as any);
-}
+export default function initStore() {
+  const middlewareList: any[] = [];
+  
+  if (config.isDev) {
+    middlewareList.push(createLogger());
+  }
 
-const composeEnhancers = (import.meta.env.MODE === 'development'
-    && window
-    && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
-  || compose;
-
-export default function initStore(): Store<RootState> {
-  return createStore(
-    rootReducer(),
-    composeEnhancers(
-      applyMiddleware(...middlewares),
-    ),
-  );
+  return configureStore({
+    reducer: rootReducer(),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(middlewareList),
+    devTools: import.meta.env.MODE === 'development',
+  });
 }
